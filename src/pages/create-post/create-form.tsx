5bfef1757a86c4af23b7b57139eef9c340ed874d
Priyanshu <sethipriyanshu@outlook.com>
2024-05-30 07:05:03 -0500

    integrate firebase to create post form

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,6 +1,10 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver} from "@hookform/resolvers/yup";
+import { addDoc, collection} from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../config/firebase";
 
 interface CreateFormData {
     title: string;
@@ -9,6 +13,7 @@ interface CreateFormData {
 
 
 export const CreateForm = () =>{
+        const [user] = useAuthState(auth);
     const schema = yup.object().shape({
         title: yup.string().required("*Title is a Required Field"),
         description: yup.string().required("*Description is a Required Field").max(200),
@@ -18,8 +23,16 @@ export const CreateForm = () =>{
     } = useForm<CreateFormData>({
         resolver: yupResolver(schema),
     });
-    const onCreatePost = (data: CreateFormData) =>{
-        console.log(data);
+
+    const postsRef = collection(db,"posts");
+
+    const onCreatePost = async (data: CreateFormData) =>{
+        addDoc(postsRef, {
+            title: data.title,
+            description: data.description,
+            username: user?.displayName,
+            id: user?.uid,
+        })
     }
     return( <form onSubmit={handleSubmit(onCreatePost)}>
         <input placeholder="Title..."{...register("title")}/>
