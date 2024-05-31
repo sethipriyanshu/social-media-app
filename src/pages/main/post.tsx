f7cbbd6e16dbd462a9c372c49b5cdb36bd06fdcf
Priyanshu <sethipriyanshu@outlook.com>
2024-05-31 05:06:16 -0500

    fix fetching post from database

---
+++ src/pages/main/post.tsx
@@ -0,0 +1,24 @@
+import { Post as Ipost } from "./main";
+
+interface Props{
+    post: Ipost;
+}
+
+export const Post = (props: Props) =>{
+    const {post} = props
+    return <div>
+        <div className="title">
+            <h1>
+                {post.title}
+            </h1>
+        </div>
+        <div className="body">
+            <p>
+                {post.description}
+            </p>
+        </div>
+        <div className="footer"><p>
+            @{post.username}
+            </p></div>
+    </div>
+}   
