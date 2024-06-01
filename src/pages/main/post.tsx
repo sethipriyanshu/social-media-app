6ddcab74c527f605014b89ae8dc39a461a090612
Priyanshu <sethipriyanshu@outlook.com>
2024-06-01 02:29:31 -0500

    add like system

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,4 +1,8 @@
 import { Post as Ipost } from "./main";
+import { addDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
 
 interface Props{
     post: Ipost;
@@ -6,6 +10,24 @@ interface Props{
 
 export const Post = (props: Props) =>{
     const {post} = props
+    const likesRef = collection(db,"likes");
+    const likesDoc = query(likesRef, where("postId","==",post.id))
+    const [user] = useAuthState(auth);
+    const [likeAmount, setLikeAmount] = useState<number | null>(null);
+
+    const getLikes = async () =>{
+        const data = await getDocs(likesDoc)
+        setLikeAmount(data.docs.length);
+    }
+    const addLike = async () =>{
+        addDoc(likesRef, {
+            userId: user?.uid,
+            postId: post.id
+        })
+    }
+    useEffect(()=>{
+        getLikes();
+    },[])
     return <div>
         <div className="title">
             <h1>
@@ -19,6 +41,9 @@ export const Post = (props: Props) =>{
         </div>
         <div className="footer"><p>
             @{post.username}
-            </p></div>
+            </p>
+            <button onClick={addLike}>&#128077;</button>
+            {likeAmount && <p>Likes: {likeAmount}</p>}    
+            </div>
     </div>
 }   
