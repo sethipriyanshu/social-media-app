f050d5211064dbd410db3b5ebe4a9067961cad8e
Priyanshu <sethipriyanshu@outlook.com>
2024-06-05 06:16:56 -0500

    add comments to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,21 @@
+// import Link to enable navigation
 import { Link } from "react-router-dom";
+// import auth  display username and image
 import { auth } from "../config/firebase";
+// import useAuthState to store the auth data in a state
 import { useAuthState } from "react-firebase-hooks/auth"
+// import signOut function from firebase to enable signing out
 import { signOut } from "firebase/auth";
 
 export const Navbar = () =>{
-
+    // declare and populate auth variable
     const [user] = useAuthState(auth);
     const signUserOut = async () =>{
         await signOut(auth);
     }
+    // return the html portion of the navbar
+    // modify the display of options in respect to the login status
+    // modify the sign in, sign out buttons based on login status
     return( <div className="navbar">
         <div className="links">
         <Link to="/">Home</Link>
