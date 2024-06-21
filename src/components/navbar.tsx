fedb9bc8513281ea5f62cfe7bae66c43d08df1c9
Priyanshu <sethipriyanshu@outlook.com>
2024-06-21 06:13:09 -0500

    install tailwind

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -33,6 +33,7 @@ export const Navbar = () =>{
         </>
       )}
         </div>
+        
     </div>
     )
 }
--- src/pages/create-post/create-post.tsx
+++ src/pages/create-post/create-post.tsx
@@ -1,4 +1,6 @@
+// import the create form element
 import { CreateForm } from "./create-form"
+// just return the UI to the website
 export const CreatePost = () =>{
     return <div className="create-post">
             <CreateForm/>
